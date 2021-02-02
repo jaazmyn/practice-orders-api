37d01a21583b83d545d1b8e1644b7471feee0032
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-02 13:58:02 +0100

    Install dotenv and create db pool

---
+++ db/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const { DBUSER, DBHOST, DATABASE, PASSWORD, PORT } = process.env;
+
+const pool = new Pool({
+  user: DBUSER,
+  host: DBHOST,
+  database: DATABASE,
+  password: PASSWORD,
+  port: PORT,
+});
+
+const db = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
+
+module.export = db;
